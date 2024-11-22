df8eb9b09619bf029c2ce2c4949b6f0b72c511ab
formateur357 <formateur357@gmail.com>
2024-11-22 13:23:29 +0100

    add exercice auth

--- todo/src/app/components/logout/logout.component.ts
+++ todo/src/app/components/logout/logout.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-logout',
   standalone: true,
   imports: [],
   templateUrl: './logout.component.html',
-  styleUrl: './logout.component.css'
+  styleUrl: './logout.component.css',
 })
 export class LogoutComponent {
-
+  constructor(public auth: AuthService) {}
 }
